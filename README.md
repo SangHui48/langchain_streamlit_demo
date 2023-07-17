ea9beadf5e04dcea43c91f2f449b7a5158c6092b
한상희 <70365377+SangHui48@users.noreply.github.com>
2023-07-17 18:00:03 +0900

    7/17

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # langchain_streamlit_demo
 랭체인 스트림릿 연습
+
+> conda create -n langflow  
+> conda activate langflow  
+> pip install -r requirements.txt
---
+++ app.py
@@ -0,0 +1,64 @@
+import os
+import pickle
+from PyPDF2 import PdfReader
+import streamlit as st
+from dotenv import load_dotenv
+from streamlit_extras.add_vertical_space import add_vertical_space
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.vectorstores import FAISS
+
+# Sidebar contents
+with st.sidebar:
+    st.title('🤗💬 LLM Chat App')
+    st.markdown('''
+    ## About
+    This app is an LLM-powered chatbot built using:
+    - [Streamlit](https://streamlit.io/)
+    - [LangChain](https://python.langchain.com/)
+    - [OpenAI](https://platform.openai.com/docs/models) LLM model
+ 
+    ''')
+    add_vertical_space(5)
+    st.write('Made with ❤️ by [Prompt Engineer](https://youtube.com/@engineerprompt)')
+
+def main():
+    st.header("Chat with PDF 💬")
+    load_dotenv()
+
+    # upload a PDF file
+    pdf = st.file_uploader("Upload your PDF", type="pdf")
+
+    if pdf is not None:
+        pdf_reader = PdfReader(pdf)
+
+        text = ""
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+        
+        text_splitter = RecursiveCharacterTextSplitter(
+            chunk_size=1000,
+            chunk_overlap=200,
+            length_function=len
+        )
+
+        chunks = text_splitter.split_text(text=text)
+        
+        # Embeddings
+        store_name = pdf.name[:-4]
+        st.write(f'{store_name}')
+
+        if os.path.exists(f"{store_name}.pkl"):
+            with open(f"{store_name}.pkl", "rb") as f:
+                VectorStore = pickle.load(f)
+            st.write("Embeddings Loaded from the Disk")
+        else:
+            embeddings = OpenAIEmbeddings()
+            VectorStore = FAISS.from_texts(chunks, embedding=embeddings)
+            with open(f"{store_name}.pkl", "wb") as f:
+                pickle.dump(VectorStore, f)
+
+
+
+if __name__ == '__main__':
+    main()
